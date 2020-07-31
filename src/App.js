5ad1fc3396409694c854b7b0fbe21882ed14c98e
Matheus Martins <mathews_2010@outlook.com>
2020-07-31 12:49:00 -0300

    todo-list

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+import Todo from './Components/Todo'
+import TodoForm from './Components/TodoForm'
+
+
+function App() {
+  const [todos, setTodos] = useState([
+    {
+      text: "Learn about React",
+      isCompleted: false
+    },
+    {
+      text: "Meet friend for lunch",
+      isCompleted: false
+    },
+    {
+      text: "Build really cool todo app",
+      isCompleted: false
+    }
+  ]);
+
+
+  const addTodo = text => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  };
+
+  const completeTodo = index => {
+      const newTodos = [...todos]
+      newTodos[index].isCompleted = true
+      setTodos(newTodos)
+  }
+
+  const removeTodo = (index) => {
+      const newTodos = [...todos]
+      newTodos.splice(index, 1)
+      setTodos(newTodos)
+  }
+
+  return (
+    <div className="app">
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <Todo
+            key={index}
+            index={index}
+            todo={todo}
+            completeTodo={completeTodo}
+            removeTodo={removeTodo}
+          />
+        ))}
+        <TodoForm addTodo={addTodo} />
+      </div>
+    </div>
+  );
+}
+  export default App;
